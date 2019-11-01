202184b84da3fc383be157e087c34a1a042212fa
stanley chiuba <stanleyregal004@yahoo.com>
2019-11-01 04:47:15 +0100

    simple node module

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+// imports rectangle module
 var rect = require("./rectangle");
 
 function solveRect(l,b){
+     // illustrating main computation, which computes without delay
   console.log("solving for rectangle with l=" +l+ "and b="+b);
-
-  if(l<=0 || b<=0){
-      console.log("Rectangle dimensions should be greater than zero:l= "+l+"and b= "+b);
-  }else{
-      console.log("Area of rectangle is "+rect.Area(l,b));
-      console.log("Perimeter of rectangle is "+ rect.perimeter(l,b));
-  }
+  // rect object handling closure property
+  rect(l,b,(err,rectangle)=>{
+      // error handling for callback object from imported module
+      if(err){
+          console.log("ERROR:",err.message);
+      }else{
+          // prints this result when the callback function does not issue an error
+          console.log("The Area of the rectangle of the rectangle with l= "
+           + l + "and b " +b+ "is" +rectangle.Area() );
+         console.log("The perimeter of the rectangle with l= "
+           + l + "and b " +b+ "is" +rectangle.perimeter() );
+      }
+  });
+  // illustrating main computation, which computes without delay
+   console.log("This statement is executed after the callback");
 }
 
 solveRect(2,5);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,21 @@
-exports.perimeter =  (x,y)=>(2*(x+y));
-exports.Area = (x,y)=>(x*y);
+// closure propery makes the parameters available in setTimeout callback function
+module.exports = (x,y,callback) => {
+    if(x<=0 || y<=0){
+        // sets the delay for 2 secs, and issues callback with first parameter
+        //an error object, second parameter is set to null a it's ignored
+        setTimeout(()=>
+        callback(new Error("Rectangle dimensions should be greater than zero:l= "+x+"and b= "+y),null),
+        2000);
+    
+  }else{
+      // sets the delay for 2 secs and prints the result for the rectangle callback
+      //object(perimeter,Area) that returned
+      setTimeout(()=>callback(null,
+      { perimeter :  ()=>(2*(x+y)),
+        Area : ()=>(x*y)}),
+   2000);
+  }
+}
+
+
+
